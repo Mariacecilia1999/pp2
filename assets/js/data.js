fdd8fd7b501e1e93762260a6177c7c3974397deb
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2024-04-26 17:13:34 -0300

    Agrego datos en formato tipo Json

---
+++ assets/js/data.js
@@ -0,0 +1,37 @@
+const data = {
+   "nombre": "Soluciones Legales",
+   "descripcion": "Servicio integral de asesoría legal para individuos y pequeñas empresas.",
+   "servicios": [
+     {
+       "nombre": "Asesoramiento Legal General",
+       "descripcion": "Consultas legales, análisis de contratos, orientación sobre temas legales.",
+       "costo": "Variable, contactar para cotización."
+     },
+     {
+       "nombre": "Gestión de Documentos Legales",
+       "descripcion": "Redacción y revisión de contratos, preparación de documentos legales.",
+       "costo": "Variable, contactar para cotización."
+     },
+     {
+       "nombre": "Representación Legal",
+       "descripcion": "Representación ante tribunales, asesoramiento en litigios.",
+       "costo": "Variable, contactar para cotización."
+     },
+     {
+       "nombre": "Servicios Especializados",
+       "descripcion": "Asesoramiento en derecho laboral, de familia y de negocios.",
+       "costo": "Variable, contactar para cotización."
+     }
+   ],
+   "contacto": {
+     "correo": "info@solucioneslegales.com",
+     "telefono": "+XX-XXXX-XXXX",
+     "direccion": "Dirección de la oficina, si es aplicable"
+   },
+   "horario_atencion": "De lunes a viernes, de 9:00 a.m. a 6:00 p.m.",
+   "fundador": {
+     "nombre": "Alberto López",
+     "experiencia": "Alberto López es un abogado graduado de la Prestigiosa Universidad de Buenos Aires, con más de 6 años de experiencia trabajando con diversos clientes. Su experiencia abarca desde el asesoramiento legal general, incluyendo consultas y análisis de contratos, hasta la representación ante tribunales y la gestión de documentos legales. Además, ha brindado asesoramiento especializado en áreas como el derecho laboral, de familia y de negocios, ayudando a individuos y empresas a resolver una amplia gama de problemas legales."
+   }
+ }
+ 
---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+
+const $ = (selector) => document.querySelector(selector)
+
+const mostrarDatos = (datos) =>{
+   console.log(datos)
+}
+
+
+
+
+const inicializador = () =>{
+   mostrarDatos(data)
+
+}
+
+window.addEventListener('load', inicializador)
