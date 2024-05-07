799d4d880e726dc702b4c9d438004c6373b072a6
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2024-05-07 14:26:43 -0300

    Mostrando servicios con js y agregando los primeros estilos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+   const servicesSection = document.createElement("section");
+   servicesSection.setAttribute("id", "servicios");
 
-const $ = (selector) => document.querySelector(selector)
+   const main = document.querySelector("main");
+   main.appendChild(servicesSection);
 
-const mostrarDatos = (datos) =>{
-   console.log(datos)
-}
+   const servicesData = data.servicios;
 
+   servicesData.forEach(service => {
+       const serviceDiv = document.createElement("div");
+       serviceDiv.classList.add("servicios");
 
+       const serviceName = document.createElement("h2");
+       serviceName.textContent = service.nombre;
 
+       const serviceDescription = document.createElement("p");
+       serviceDescription.textContent = service.descripcion;
 
-const inicializador = () =>{
-   mostrarDatos(data)
+       const serviceCost = document.createElement("p");
+       serviceCost.textContent = "Costo: " + service.costo;
 
-}
+       serviceDiv.appendChild(serviceName);
+       serviceDiv.appendChild(serviceDescription);
+       serviceDiv.appendChild(serviceCost);
 
-window.addEventListener('load', inicializador)
+       servicesSection.appendChild(serviceDiv);
+   });
+});
