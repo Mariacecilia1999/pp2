6821e2be83461391b90bae0aa3c973711a105a2f
Mariacecilia1999 <mariaceciliaconti1999@gmail.com>
2024-05-09 14:20:40 -0300

    Agrego funcion para mostrar los servicios de manera dinamica con literalString

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,29 +3,36 @@ const $$ = selector => document.querySelectorAll(selector)
 
 
 const showDataAbout = (data) =>{
-    console.log(data)
-
     Object.entries(data).forEach(([key, value]) =>{
-        console.log(data)
         if(key === 'experiencia'){
-            console.log(value)
             $('#about').innerHTML += `<div class="about">
                                         <p>${value}</p>
                                         <img src="./assets/img/man-5806012_640.jpg" alt="Imagen de abogado firmando">
                                     </div>`
         }
-       
-
     })   
     }
 
 
 
-
+const showDataServices = (data) =>{
+    for(const service in data){
+        const dataObj = data[service]
+        $('.servicesCard').innerHTML += `<div class='card'>
+                                            <h3 class='titleCard'>${dataObj.nombre}<h3/>
+                                            <div class='textsCard'>
+                                                <p>${dataObj.descripcion}</p>
+                                                <p class='cost'>${dataObj.costo}</p>
+                                            </div>
+                                        </div>`
+        
+    }
+}
 
 
 const  initializer = () =>{
     showDataAbout(data.fundador)
+    showDataServices(data.servicios)
 }
 
 
